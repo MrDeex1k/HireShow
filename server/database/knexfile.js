c71d9f2954fd9092dcb6b5a1a6359277580773e4
Jakub Batycki <jakub.batycki.jr@gmail.com>
2025-09-16 10:35:33 +0200

    Fix for DB conf

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -4,6 +4,20 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env'
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
   production: {
     client: process.env.DB_CLIENT,
     connection: {
